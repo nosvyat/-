94a49ccd0bfce21d9adb791b918cd7560cec5cbb
nosvyat <52301232+nosvyat@users.noreply.github.com>
2026-03-29 23:15:33 +0600

    Refactor main.js to integrate game functions

--- js/main.js
+++ js/main.js
@@ -1,39 +1,14 @@
-import { state } from "./state.js";
-import { updateUI } from "./ui.js";
+import { updateGame, runPassiveIncome, tapEnergy } from "./game.js";
 
 import "./shop.js";
 import "./upgrades.js";
 
 const tapBtn = document.getElementById("tapBtn");
 
-function recalculateIncome() {
-  const starIncome = state.stars * state.starLevel * 2;
-  const planetIncome = state.planets * state.planetLevel * 1;
-
-  state.incomePerSecond = starIncome + planetIncome;
-}
-
-function tapEnergy() {
-  state.energy += state.tapPower;
-  updateGame();
-}
-
-function gameLoop() {
-  state.energy += state.incomePerSecond / 2;
-  updateUI();
-}
-
-function updateGame() {
-  recalculateIncome();
-  updateUI();
-}
-
 tapBtn.addEventListener("click", tapEnergy);
 
 updateGame();
 
 setInterval(() => {
-  gameLoop();
+  runPassiveIncome();
 }, 500);
-
-export { updateGame, recalculateIncome };
