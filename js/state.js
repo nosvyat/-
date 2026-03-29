b8d8bebd21c557af0b3d673a3253f415818cc511
nosvyat <52301232+nosvyat@users.noreply.github.com>
2026-03-29 23:02:34 +0600

    Initialize state object with game parameters

---
+++ js/state.js
@@ -0,0 +1,21 @@
+export const state = {
+  energy: 0,
+  incomePerSecond: 0,
+
+  universeLevel: 1,
+
+  stars: 0,
+  planets: 0,
+
+  starLevel: 1,
+  planetLevel: 1,
+
+  starCost: 20,
+  planetCost: 35,
+
+  starUpgradeCost: 30,
+  planetUpgradeCost: 28,
+  universeUpgradeCost: 60,
+
+  tapPower: 1
+};
